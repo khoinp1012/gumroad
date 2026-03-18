bbb276614ab23cc6bbb4eeaee4673aa0d3bbfef1
gumclaw <gumclaw@gumroad.com>
2026-03-18 11:46:35 -0400

    Use atomic cache delete for notify-buyer flag
    Prevents duplicate 'files ready' emails when long and critical
    queue jobs race on the same purchase.

--- app/sidekiq/stamp_pdf_for_purchase_job.rb
+++ app/sidekiq/stamp_pdf_for_purchase_job.rb
@@ -12,9 +12,8 @@ class StampPdfForPurchaseJob
     purchase = Purchase.find(purchase_id)
     PdfStampingService.stamp_for_purchase!(purchase)
 
-    if Rails.cache.read(PdfStampingService.notify_buyer_cache_key(purchase_id))
+    if Rails.cache.delete(PdfStampingService.notify_buyer_cache_key(purchase_id))
       CustomerMailer.files_ready_for_download(purchase_id).deliver_later(queue: "critical")
-      Rails.cache.delete(PdfStampingService.notify_buyer_cache_key(purchase_id))
       Rails.cache.delete(PdfStampingService.cache_key_for_purchase(purchase_id))
     end
   end
